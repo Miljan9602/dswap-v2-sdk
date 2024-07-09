17d0b09dcb347e10bd2d0d1ff603cfae8680ff8a
Miljan9602 <rakitamiljan@yahoo.com>
2024-07-09 20:26:43 +0200

    Updated versions

---
+++ dist/constants.d.ts
@@ -0,0 +1,19 @@
+import { Percent } from '@miljan9602/dswap-sdk-core';
+import JSBI from 'jsbi';
+/**
+ * @deprecated use FACTORY_ADDRESS_MAP instead
+ */
+export declare const FACTORY_ADDRESS = "0x5D370a6189F89603FaB67e9C68383e63F7B6A262";
+export declare const FACTORY_ADDRESS_MAP: {
+    [chainId: number]: string;
+};
+export declare const INIT_CODE_HASH = "0x7240a7a71cc85d56ee2e5ca1c5236ce877abc9b49b4696adc94968ef43796fed";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
+export declare const BASIS_POINTS: JSBI;
+export declare const ZERO_PERCENT: Percent;
+export declare const ONE_HUNDRED_PERCENT: Percent;
